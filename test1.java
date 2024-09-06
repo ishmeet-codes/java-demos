e1e61510ba184f551ea2e663c840b580b9fb59c7
ishmeet-codes <ishmeetsinghxa@gmail.com>
2024-09-07 00:16:42 +0530

    done que1

--- test1.java
+++ test1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class test1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter marks of subject 1 :- ");
+        float m1 = sc.nextFloat();
+        System.out.print("Enter marks of subject 2 :- ");
+        float m2 = sc.nextFloat();
+        System.out.print("Enter marks of subject 3 :- ");
+        float m3 = sc.nextFloat();
+        System.out.print("Enter marks of subject 4 :- ");
+        float m4 = sc.nextFloat();
+        System.out.print("Enter marks of subject 5 :- ");
+        float m5 = sc.nextFloat();
+        float total = (m1+m2+m3+m4+m5)/5;
+        System.out.print("Total percentage is :- ");
+        System.out.println(total);
+
 
     }
 }
