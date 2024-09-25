0d940f4735198a829890eee0b1eb6f4c65f0ed16
ishmeet-codes <ishmeetsinghxa@gmail.com>
2024-09-26 00:29:53 +0530

    while-loop

---
+++ loop.java
@@ -0,0 +1,12 @@
+public class loop {
+    public static void main(String[] args) {
+        int i=1;
+        System.out.println("i = 1");
+        while(i<=9){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While loop over");
+
+    }
+}
